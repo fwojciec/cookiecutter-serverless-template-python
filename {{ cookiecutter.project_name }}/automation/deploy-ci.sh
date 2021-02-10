2dfc8853d6944d95d19586c978b701f7e22fa0df
Filip Wojciechowski <fwojciec@gmail.com>
2021-02-10 22:11:10 +0100

    initial commit

---
+++ {{ cookiecutter.project_name }}/automation/deploy-ci.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -euo pipefail
+
+STACK_NAME={{ cookiecutter.project_name }}-ci
+
+CLOUDFORMATION_ARTIFACTS_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name==\`CloudformationArtifactsBucket\`].Value" --output text)
+if [ -z "$CLOUDFORMATION_ARTIFACTS_BUCKET" ]; then
+  echo "Unable to locate Cloudformation Export 'CloudformationArtifactsBucket' - have you setup the account-wide-resources for this account and region?"
+  exit 1
+fi
+
+sam deploy \
+        --stack-name $STACK_NAME \
+        --template-file ci.yaml \
+        --capabilities CAPABILITY_IAM \
+        --parameter-overrides CloudformationArtifactsBucket=$CLOUDFORMATION_ARTIFACTS_BUCKET \
+        --no-fail-on-empty-changeset
